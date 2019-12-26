d18eb085a53cbbd9d8acb4bf4f105bfcc1313e53
ryosuke.aoki <ryo1018@live.jp>
2019-12-26 13:34:27 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 青木亮祐 on 2019/12/26.
+//  Copyright © 2019 ryosuke.aoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
